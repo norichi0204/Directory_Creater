b7a1c08ca9cd937f910dd799cd23f70db62c285b
ぱんくん <yasu.0204nori@outlook.jp>
2022-04-09 17:54:12 +0900

    プロジェクト ファイルを追加します。

---
+++ フォルダ作成ツール/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace フォルダ作成ツール
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const string 大フォルダ = "成果物\\";
+        
+            StreamReader sr = new StreamReader("フォルダ名.txt", Encoding.GetEncoding("Shift_JIS"));
+
+            while (sr.Peek() != -1)
+            {          
+                Directory.CreateDirectory(大フォルダ + sr.ReadLine());
+            }
+
+            sr.Close();
+
+           
+        }
+    }
+}
